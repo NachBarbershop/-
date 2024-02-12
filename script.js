12d608cda9ab9e31076f61334e69fe12cedaa2af
NachBarbershop <158759471+NachBarbershop@users.noreply.github.com>
2024-02-12 02:18:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.getElementById('miBoton').addEventListener('click', function() {
-    // Lógica que se ejecutará al hacer clic en el botón
-    alert('¡Botón clicado!');
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('open');
+}
+
+// Cerrar el menú si se toca nuevamente
+var menuLinks = document.querySelectorAll('.header-li-a');
+menuLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        var menu = document.getElementById('menu');
+        menu.classList.remove('open');
+    });
+});
+
+// Cerrar el menú si se toca fuera del menú
+document.addEventListener('click', function(event) {
+    var menu = document.getElementById('menu');
+    var menuButton = document.querySelector('.header-menu');
+    if (!menu.contains(event.target) && event.target !== menuButton) {
+        menu.classList.remove('open');
+    }
 });
