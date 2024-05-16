dfd367872ae3f5efa311d005c5a542c631563b75
NachBarbershop <158759471+NachBarbershop@users.noreply.github.com>
2024-05-15 22:36:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// Función para alternar el menú abierto y cerrado
 function toggleMenu() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('open');
 }
 
-// Cerrar el menú si se toca nuevamente
+// Cerrar el menú si se toca nuevamente en un enlace del menú
 var menuLinks = document.querySelectorAll('.header-li-a');
 menuLinks.forEach(function(link) {
     link.addEventListener('click', function() {
@@ -12,7 +13,7 @@ menuLinks.forEach(function(link) {
     });
 });
 
-// Cerrar el menú si se toca fuera del menú
+// Cerrar el menú si se toca fuera del menú o del botón de menú
 document.addEventListener('click', function(event) {
     var menu = document.getElementById('menu');
     var menuButton = document.querySelector('.header-menu');
@@ -20,3 +21,29 @@ document.addEventListener('click', function(event) {
         menu.classList.remove('open');
     }
 });
+
+// Observador de intersección
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1 // Cambiamos el umbral para activar la observación
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+// Elementos a observar
+let items = document.querySelectorAll('.item');
+items.forEach(item => {
+    observer.observe(item);
+});
+
+// Función de devolución de llamada
+function callback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+}
